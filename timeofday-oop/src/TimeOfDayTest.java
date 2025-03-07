6be3034320d9f3d65ac69bf4e49c5b7fb9b4edad
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2025-03-07 11:05:57 +0100

    Test suite

---
+++ timeofday-oop/src/TimeOfDayTest.java
@@ -0,0 +1,25 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TimeOfDayTest {
+
+	@Test
+	void test() {
+		TimeOfDay myTimeOfDay = new TimeOfDay(10, 30);
+		assertEquals(10, myTimeOfDay.getHours());
+		assertEquals(30, myTimeOfDay.getMinutes());
+		assertEquals(630, myTimeOfDay.getMinutesSinceMidnight());
+		
+		myTimeOfDay.setHours(13);
+		assertEquals(13, myTimeOfDay.getHours());
+		assertEquals(30, myTimeOfDay.getMinutes());
+		assertEquals(13*60 + 30, myTimeOfDay.getMinutesSinceMidnight());
+		
+		myTimeOfDay.setMinutes(0);
+		assertEquals(13, myTimeOfDay.getHours());
+		assertEquals(0, myTimeOfDay.getMinutes());
+		assertEquals(13*60, myTimeOfDay.getMinutesSinceMidnight());
+	}
+
+}
